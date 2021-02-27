95eac9dc670cb3f5f1dca6a42d923198b5559326
dkaczmarzyk <dam.kaczmarzyk@gmail.com>
2021-02-27 16:50:01 +0100

    Add registered user to vector

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    KsiazkaAdresowa ksiazkaAresowa;
+    ksiazkaAresowa.rejestracjaUzytkownika();
+    ksiazkaAresowa.wypiszWszystkichUzytkownikow();
     return 0;
 }

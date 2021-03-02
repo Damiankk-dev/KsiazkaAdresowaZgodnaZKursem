1b75c2fba4ef981816d24d18851db92561690ba0
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-03-02 18:32:52 +0100

    Load users from file at program start

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,20 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+
+using namespace std;
+
+class MetodyPomocnicze{
+public:
+    string konwerjsaIntNaString(int liczba);
+    string wczytajLinie();
+};
+
+
+
+#endif // METODYPOMOCNICZE_H

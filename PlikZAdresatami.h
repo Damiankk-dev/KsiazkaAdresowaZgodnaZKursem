bafc3f9f7824a3fc81c18ef42c57c9d927f25d0c
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-03-09 17:50:47 +0100

    Add files for class PlikZAdresatami

---
+++ PlikZAdresatami.h
@@ -0,0 +1,18 @@
+#ifndef PLIKZADRESATAMI_H
+#define PLIKZADRESATAMI_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "Uzytkownik.h"
+#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class PlikZAdresatami{
+
+    const string nazwaPlikuZUzytkownikami;
+
+};
+#endif // PLIKZADRESATAMI_H
